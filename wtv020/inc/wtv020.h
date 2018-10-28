c0808e28e7ef01299082deaa3e60152905b731b4
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2018-10-27 21:22:22 -0300

    Add files via upload

--- wtv020/inc/wtv020.h
+++ wtv020/inc/wtv020.h
@@ -1,14 +1,14 @@
 /*
  * Esta biblioteca comanda o módulo WTV02 no modo SPL
  *
- * wtv02.h
+ * wtv020.h
  *
  *  Created on: 27 de out de 2018
  *      Author: Eng. Fabricio de Lima Ribeiro
  *
  *  no arquivo main.c incluir os arquivos na sequencia:
     #include "delays.h"
-    #include "wtv02.h"
+    #include "wtv020.h"
  */
 //Dependências: delays.h
 
@@ -16,14 +16,14 @@
 #define WTV020_H_
 
 //definição dos pinos de controle
-#define	WTV02_RESET GPIO_Pin_0 //PA0-RESET
-#define	WTV02_CLOCK GPIO_Pin_1 //PA1-CLOCK
-#define WTV02_DATA  GPIO_Pin_2 //PA2-DATA
-#define WTV02_PORT  GPIOA
+#define	WTV020_RESET GPIO_Pin_0 //PA0-RESET
+#define	WTV020_CLOCK GPIO_Pin_1 //PA1-CLOCK
+#define WTV020_DATA  GPIO_Pin_2 //PA2-DATA
+#define WTV020_PORT  GPIOA
 
 //definição do comando stop e volume
-#define WTV02_STOP   0xFFFF
-#define WTV02_VOLUME 0xFFF5; //configura o volume 5
+#define WTV020_STOP   0xFFFF
+#define WTV020_VOLUME 0xFFF5; //configura o volume 5
 
 //funções
 extern void Delay_ms(const int msec); //delay ms
